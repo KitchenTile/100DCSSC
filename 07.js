71cd08f270dd2c81856a57505d97ea2415965a5f
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-12-31 18:58:34 +0000

    Create 07.js

---
+++ 07.js
@@ -0,0 +1,8 @@
+$(".menu-icon").bind("click", function() {
+	$(".container").toggleClass("menu-active");
+		$(".menu").toggleClass("menu-active");
+})
+
+$(".search-icon").bind("click", function() {
+	$(".search-bar").toggleClass("active");
+})
