088644ca21e879af618bcaf105958246992d046b
Blue <118444162+KitchenTile@users.noreply.github.com>
2025-01-07 16:35:12 +0000

    Create 13.js

---
+++ 13.js
@@ -0,0 +1,7 @@
+$(".close-bttn").bind("click", function() {
+	$(".profile-in").toggleClass("active");
+})
+
+$(".to-profile-bttn").bind("click", function() {
+	$(".profile-in").toggleClass("active");
+})
