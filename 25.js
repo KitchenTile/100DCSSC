6477484751806b936ccaea962b1984303d66b7d6
Blue <118444162+KitchenTile@users.noreply.github.com>
2025-01-19 13:56:25 +0000

    Create 25.js

---
+++ 25.js
@@ -0,0 +1,8 @@
+$(".pin").bind("click", function() {	$(".pin").toggleClass("active")
+														$(".card").toggleClass("active")
+})
+
+
+$(".card").bind("click", function() {	$(".pin").toggleClass("active")
+														$(".card").toggleClass("active")
+})
